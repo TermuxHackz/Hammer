f6baa8ebac86fc9c9adc3c6ed1cc5ad4f94f396e
ASLEFNDAR <138137089+ASLEFNDAR@users.noreply.github.com>
2023-12-03 09:25:08 +0200

    DDOS.py

--- DDOS.py
+++ DDOS.py
@@ -90,10 +90,10 @@ def dos2():
 
 
 def usage():
-	print (''' \033[92m this tool is programed by ASLEFNDAR
+	print (''' \033[92m	ASLEFNDAR-DDos Attack Tool v1.0
 	It is the end user's responsibility to obey all applicable laws.
 	It is just for server testing script. Your ip is visible. \n
-	usage : python3 hammer.py [-s] [-p] [-t]
+	usage : python3 DDOS.py [-s] [-p] [-t]
 	-h : help
 	-s : server ip
 	-p : port default 80
